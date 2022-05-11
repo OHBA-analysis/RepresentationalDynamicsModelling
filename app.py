73fd529f408ab99ebd614074441a25bff10f2e09
matsvanes <mats.vanes@psych.ox.ac.uk>
2022-05-11 12:43:50 +0100

    replace matplotlib by pyplot

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ inspired by M. Fabus (https://gitlab.com/marcoFabus/fabus2022_harmonics/-/blob/m
 """
 
 import plotly
+from plotly.subplots import make_subplots
+import plotly.graph_objs as go
+
+import plotly.express as px
 import matplotlib.pyplot as plt
-import matplotlib
-matplotlib.use('Agg')
 import dash
 from dash.dependencies import Input, Output, State
 import numpy as np
@@ -22,7 +24,7 @@ def card_amp(header, name, props):
             [
                 html.H4(header, className="card-title", id=header),
                 html.Div(
-                    style={'width': '90%'},
+                    style={'width': '100%'},
                     children=[
 
                         dbc.Row(
@@ -41,7 +43,7 @@ def card_amp(header, name, props):
                                 dbc.Col(
                                     [
                                         html.Div(
-                                            style={'padding-right': '0%', 'width': '120%'},
+                                            style={'padding-right': '0%', 'width': '100%'},
                                             children=[dcc.Slider(
                                                 id=name[0],
                                                 min=props[0],
@@ -74,7 +76,6 @@ def card_amp(header, name, props):
                                                     step=props[3],
                                                     marks=props[4],
                                                 ), ])
-
                                     ],
                                     width=8)
                             ])
@@ -93,7 +94,7 @@ def card_freq(header, name, props):
             [
                 html.H4(header, className="card-title", id=header),
                 html.Div(
-                    style={'width': '90%'},
+                    style={'width': '100%'},
                     children=[
 
                         dbc.Row(
@@ -110,7 +111,7 @@ def card_freq(header, name, props):
                                 dbc.Col(
                                     [
                                         html.Div(
-                                            style={'padding-right': '0%', 'width': '120%'},
+                                            style={'padding-right': '0%', 'width': '100%'},
                                             children=[dcc.Slider(
                                                 id=name[0],
                                                 min=props[0],
@@ -226,8 +227,8 @@ def toggle_modal(n, is_open):
 
 
 @app.callback(
-    [Output('graph', 'figure')],
-    # Output('example_button', component_property='value')],
+    [Output('graph', 'figure'),
+     Output('example_button', component_property='value')],
     [Input(x, 'value') for x in names] +
     [Input(component_id='example_button', component_property='value')], )
 def update_figure(f1=10, f2=0, s1ch1f1=1.5, s1ch1f2=0, s1ch2f1=0.75, s1ch2f2=0, s2ch1f1=0, s2ch1f2=0, s2ch2f1=0,
@@ -275,10 +276,9 @@ def update_figure(f1=10, f2=0, s1ch1f1=1.5, s1ch1f2=0, s1ch2f1=0.75, s1ch2f2=0, 
         ticks_mi = np.linspace(0, 1.5, 4)
         ticks_mipow = np.linspace(0, 0.5, 6)
 
-        ylim1 = (0, 1.5)
+        ylim1 = (0, 1.6)
         ylim2 = (0, 0.5)
 
-    example = int(0)
     theta = np.array([[-np.pi / 2, -np.pi / 2], [-np.pi / 2, -np.pi / 2]])
     theta0 = theta
     s = np.array([0.5, 0.5])  # noise for channel 1 and channel 2
@@ -367,62 +367,129 @@ def update_figure(f1=10, f2=0, s1ch1f1=1.5, s1ch1f2=0, s1ch2f1=0.75, s1ch2f2=0, 
         ticks_mipow = np.linspace(0, np.ceil(np.max(r_b)), int(2 * np.ceil(np.max(r_b)) + 1))
         ylim1 = (0, np.ceil(np.max(infotermest)))
         ylim2 = (0, np.ceil(np.max(r_b)))
+    example = int(0)
 
     # %% Plot everything
-    fig, ax = plt.subplots(3, 2)
+    import plotly.graph_objs as go
+    from plotly.subplots import make_subplots
+    fig = make_subplots(rows=3, cols=2,
+                        subplot_titles=("Stimulus Evoked Signals", "Power", "", "", "Information Content", ""))
     for k in range(3):
-        plt.sca(ax[k, 0])
         if k < 2:
-            plt.plot(t, x0[k], 'k', linewidth=2)
-            plt.fill_between(t, x0[k] - s[k], x0[k] + s[k], facecolor='gray', alpha=0.3)
-            plt.plot(t, xa[k], 'b', linewidth=2)
-            plt.fill_between(t, xa[k] - s[k], xa[k] + s[k], facecolor='b', alpha=0.3)
-            ax[k, 0].set_box_aspect(1)
-            ax[k, 0].set_yticks(np.linspace(-np.ceil(np.max(xa[k] + s[k])), np.ceil(np.max(xa[k] + s[k])), 21),
-                                minor=True)
-            ax[k, 0].set_xticks(ticks_time)
-            ax[k, 0].set_yticks(np.linspace(-np.ceil(np.max(xa[k] + s[k])), np.ceil(np.max(xa[k] + s[k])), 5))
-            ax[k, 0].tick_params(which='minor', bottom=False, left=False)
-            plt.ylabel('Magnitude')
-            plt.ylim((-2, 2))
+            fig.add_trace(
+                go.Scatter(
+                    x=t,
+                    y=x0[k],
+                    line=dict(color='black'),
+                    mode='lines',
+                    showlegend=False
+                ),
+                row=k + 1,
+                col=1
+            )
+            fig.add_trace(
+                go.Scatter(
+                    x=t.tolist() + t.tolist()[::-1],  # x, then x reversed
+                    y=(x0[k] + s[k]).tolist() + (x0[k] - s[k])[::-1].tolist(),  # upper, then lower reversed
+                    fill='toself',
+                    fillcolor='gray',
+                    opacity=0.3,
+                    line=dict(color='rgba(255,255,255,0)'),
+                    hoverinfo="skip",
+                    showlegend=False
+                ),
+                row=k + 1,
+                col=1
+            )
+            fig.add_trace(
+                go.Scatter(
+                    x=t,
+                    y=xa[k],
+                    line=dict(color='blue'),
+                    mode='lines',
+                    showlegend=False
+                ),
+                row=k + 1,
+                col=1
+            )
+            fig.add_trace(
+                go.Scatter(
+                    x=t.tolist() + t.tolist()[::-1],  # x, then x reversed
+                    y=(xa[k] + s[k]).tolist() + (xa[k] - s[k])[::-1].tolist(),  # upper, then lower reversed
+                    fill='toself',
+                    fillcolor='blue',
+                    opacity=0.3,
+                    line=dict(color='rgba(255,255,255,0)'),
+                    hoverinfo="skip",
+                    showlegend=False
+                ),
+                row=k + 1,
+                col=1
+            )
+            # fig.update_layout(xaxis=dict(title_text="Time (s)"), yaxis=dict(title_text="Magnitude"))
+            fig.update_yaxes(dict(title_text=f"CHANNEL {k + 1} <br> Magnitude"), range=[-2, 2], row=k + 1, col=1)
         else:
-            plt.plot(t, np.squeeze(infotermest), linewidth=2, color='k')
-            plt.xlim((0, np.max(t)))
-            ax[k, 0].set_box_aspect(1)
-            ax[k, 0].set_xticks(ticks_time)
-            ax[k, 0].set_yticks(ticks_mi)
-            ax[k, 0].tick_params(which='minor', bottom=False, left=False)
-            plt.ylabel('f^-1 (I(X,Y))')
-            plt.ylim(ylim1)
+            fig.add_trace(
+                go.Scatter(
+                    x=t,
+                    y=infotermest,
+                    line=dict(color='black'),
+                    mode='lines',
+                    showlegend=False
+                ),
+                row=k + 1,
+                col=1
+            )
+            fig.update_xaxes(dict(title_text="Time (s)"), row=k + 1, col=1)
+            fig.update_yaxes(dict(title_text="f<sup>-1</sup> (I<sub>(X,Y)</sub>)"), range=ylim1, row=k + 1, col=1)
 
-        plt.xlabel('Time')
-        plt.sca(ax[k, 1])
+        # right side of the figure
         if k < 2:
-            if np.any(a0[k, :] > 0):
-                markerline, stemlines, baseline = plt.stem(
-                    f[a0[k, :] > 0], a0[k, a0[k, :] > 0], linefmt='k', markerfmt='ko', basefmt='w')
-                markerline.set_markerfacecolor('none')
-            if np.any(a[k, :] > 0):
-                markerline, stemlines, baseline = plt.stem(
-                    f[a[k, :] > 0], a[k, a[k, :] > 0], linefmt='b', markerfmt='bo', basefmt='w')
-                markerline.set_markerfacecolor('none')
-            plt.ylim(ylim0)
-            ax[k, 1].set_box_aspect(1)
-            ax[k, 1].set_yticks(ticks_pow)
+            for i in np.where(a0[k, :] > 0)[0]:
+                fig.add_trace(
+                    go.Scatter(
+                        x=[f[i], f[i]],
+                        y=[-1, a0[k, i]],
+                        mode='lines+markers',
+                        line=dict(color='black', width=3),
+                        marker=dict(size=10, color='black', symbol='circle'),
+                        showlegend=False
+                    ),
+                    row=k + 1,
+                    col=2
+                )
+            for i in np.where(a[k, :] > 0)[0]:
+                fig.add_trace(
+                    go.Scatter(
+                        x=[f[i], f[i]],
+                        y=[-1, a[k, i]],
+                        mode='lines+markers',
+                        line=dict(color='blue', width=3),
+                        marker=dict(size=10, color='blue', symbol='circle'),
+                        showlegend=False
+                    ),
+                    row=k + 1,
+                    col=2
+                )
+            fig.update_yaxes(dict(title_text=f"PSD"), range=ylim0, row=k + 1, col=2)
+            fig.update_xaxes(range=[0, 40], row=k + 1, col=2)
         else:
-            markerline, stemlines, baseline = plt.stem(
-                freqs_all[r_b > 0], r_b[r_b > 0], linefmt='k', markerfmt='ko', basefmt='w')
-            ax[k, 1].set_box_aspect(1)
-            ax[k, 1].set_yticks(ticks_mipow)
-            plt.ylim(ylim2)
-            markerline.set_markerfacecolor('none')
-        ax[k, 1].set_xticks(ticks_freq)
-        ax[k, 1].tick_params(which='minor', bottom=False, left=False)
-        plt.xlabel('Frequency (Hz)')
-        plt.ylabel('PSD')
-
-    return [fig]
-
+            for i in np.where(r_b > 0)[0]:
+                fig.add_trace(
+                    go.Scatter(
+                        x=[freqs_all[i], freqs_all[i]],
+                        y=[-1, r_b[i]],
+                        mode='lines+markers',
+                        line=dict(color='black', width=3),
+                        marker=dict(size=10, color='black', symbol='circle'),
+                        showlegend=False
+                    ),
+                    row=k + 1,
+                    col=2
+                )
+                fig.update_xaxes(dict(title_text="Frequency (Hz)"), range=[0, 40], row=k + 1, col=2)
+                fig.update_yaxes(dict(title_text="PSD"), range=ylim2, row=k + 1, col=2)
+    return [fig, example]
 
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', debug=True)
