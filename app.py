3594998b0e6e9cd946cf12726266b19e5de5305d
Mats Van Es (transfer) <psyc1435_transfer@OPJMBPr15-17.local>
2022-05-25 12:34:34 +0100

    add DOIs and authorship to Instructions

--- app.py
+++ app.py
@@ -145,7 +145,9 @@ instructions = html.Div(
                 dbc.ModalHeader(dbc.ModalTitle("Instructions")),
                 dbc.ModalBody(
                     html.Div([
-                        html.A("Accompanying publication",
+                        html.A("This app is hosting Van Es (2022)",
+                               href='https://doi.org/10.5281/zenodo.6579997', target="_blank"),
+                        html.A("The simulator is based on the work in Higgins et al (2022)",
                                href='https://doi.org/10.1101/2022.02.07.479399', target="_blank"),
                         html.H5(""),
                         html.P(
@@ -163,8 +165,9 @@ instructions = html.Div(
                             We can see that when we use instantaneous signal decoding, the\
                             information content oscillates with twice the original frequency."),
                         html.H5(" "),
-                        html.P("\
-                            App created by Mats W.J. van Es, 2022, Copyright University of Oxford"),
+                        html.P("\ App created by Mats W.J. van Es, 2022, Copyright University of Oxford \
+                               Please cite as: \
+                               Van Es, M.W.J. (2022). Representational Dynamics Simulator. Zenodo. https://doi.org/10.5281/zenodo.6579997"),
                     ])),
             ],
             id="modal-fs",
